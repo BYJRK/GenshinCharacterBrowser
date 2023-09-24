91560f5a84a7f779894d2df00d56e4a02ac14f2b
BYJRK <suomikoto@outlook.com>
2023-09-24 20:14:00 +0800

    Add Fortnite

--- src/ViewModels/MainWindowViewModel.cs
+++ src/ViewModels/MainWindowViewModel.cs
@@ -61,19 +61,21 @@ public partial class MainWindowViewModel : ObservableObject
     {
         var dawnUrl = id switch
         {
-            "150" => @"https://uploadstatic.mihoyo.com/contentweb/20200211/2020021114220951905.jpg",
-            "151" => @"https://uploadstatic.mihoyo.com/contentweb/20200515/2020051511073340128.jpg",
-            "324" => @"https://uploadstatic.mihoyo.com/contentweb/20210719/2021071917030766463.jpg",
-            "350" => @"https://webstatic.mihoyo.com/upload/contentweb/2022/08/15/04d542b08cdee91e5dabfa0e85b8995e_8653892990016707198.jpg",
+            "150" => "https://uploadstatic.mihoyo.com/contentweb/20200211/2020021114220951905.jpg",
+            "151" => "https://uploadstatic.mihoyo.com/contentweb/20200515/2020051511073340128.jpg",
+            "324" => "https://uploadstatic.mihoyo.com/contentweb/20210719/2021071917030766463.jpg",
+            "350" => "https://webstatic.mihoyo.com/upload/contentweb/2022/08/15/04d542b08cdee91e5dabfa0e85b8995e_8653892990016707198.jpg",
+            "358" => "https://act-webstatic.mihoyo.com/upload/contentweb/hk4e/34ec75c9ed70f793cdd698ad1a4764e5_731983624099835302.jpg",
             _ => throw new ArgumentException(id)
         };
 
         var duskUrl = id switch
         {
-            "150" => @"https://uploadstatic.mihoyo.com/contentweb/20200211/2020021114221470532.jpg",
-            "151" => @"https://uploadstatic.mihoyo.com/contentweb/20200515/2020051511072867344.jpg",
-            "324" => @"https://uploadstatic.mihoyo.com/contentweb/20210719/2021071917033032133.jpg",
-            "350" => @"https://webstatic.mihoyo.com/upload/contentweb/2022/08/15/ab72edd8acc105904aa50da90e4e788e_2299455865599609620.jpg",
+            "150" => "https://uploadstatic.mihoyo.com/contentweb/20200211/2020021114221470532.jpg",
+            "151" => "https://uploadstatic.mihoyo.com/contentweb/20200515/2020051511072867344.jpg",
+            "324" => "https://uploadstatic.mihoyo.com/contentweb/20210719/2021071917033032133.jpg",
+            "350" => "https://webstatic.mihoyo.com/upload/contentweb/2022/08/15/ab72edd8acc105904aa50da90e4e788e_2299455865599609620.jpg",
+            "358" => "https://act-webstatic.mihoyo.com/upload/contentweb/hk4e/3ce8f43e9de08e1988aafc00fdff2410_8142185104639306099.jpg",
             _ => throw new ArgumentException(id)
         };
 
